06d6375a62629c9cb0ca0843cd60e752df9f2aaf
SravanthiN4 <snedunoori44@gmail.com>
2022-05-03 08:54:01 -0500

    starting to work on app.js and css

---
+++ src/components/Activities.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Activities = () => {
+    return (
+       
+        <div>
+            Welcome to Activities Page!!
+        </div>
+       
+    );
+};
+
+export default Activities;
---
+++ src/components/Routines.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Routines = () => {
+    return (
+       
+        <div>
+            Welcome to Routines Page!!
+        </div>
+       
+    );
+};
+
+export default Routines;
