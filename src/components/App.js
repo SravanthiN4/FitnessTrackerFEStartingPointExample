365f7f7b534521634bcca09e33dfcfd9a5b8d8d9
ConnorEndsley <endsleyconnor95@gmail.com>
2022-05-05 20:43:21 -0500

    cleaned up some white space in App

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,6 @@ const App = () => {
  const [routines, setRoutines] = useState([]);
  const [activities, setActivities] = useState([]);
 
-//  userEffect(()=> {
-//      allActivities = await getAllActivities;
-     
-//  })
-
  const [name, setName] = useState("");
  const [goal,setGoal] = useState("");
  const [isPublic,setIsPublic] = useState(false);
@@ -26,80 +21,56 @@ const App = () => {
  const [username, setUsername] = useState('');
  const [password, setPassword] = useState('');
 
-
-    const [loggedIn, setLoggedIn] = useState(false);
-
-
-    useEffect(() => {
-        setLoggedIn(!!localStorage.getItem("UserToken"))
-    }, []);
+const [loggedIn, setLoggedIn] = useState(false);
 
 
-    const logOut = () => {
-        localStorage.removeItem("UserToken");
-        setLoggedIn(false);
+useEffect(() => {
+    setLoggedIn(!!localStorage.getItem("UserToken"))
+}, []);
 
-    }
 
-    return (<div className='app'>
+const logOut = () => {
+    localStorage.removeItem("UserToken");
+    setLoggedIn(false);
+}
 
-        
-            <BrowserRouter>
-                <div id="header">
-                <h1 className='header'>Fitness Tracker</h1>
-                    <div id= "buttonRoutesBox">
+return (<div className='app'>
+
+    <BrowserRouter>
+        <div id="header">
+            <h1 className='header'>Fitness Tracker</h1>
+                <div id= "buttonRoutesBox">
                     {!loggedIn? <>
-                     <button className="button"><Link id='link' to="/login">Login</Link></button>
-                     <button className="button"><Link id='link' to="/signUp">SignUp</Link></button>
-                     <button className="button"><Link id='link' to="/routines">Public Routines</Link></button> 
-                     <button className="button"><Link id='link' to="/activities">Public Activities</Link></button>
-                     </> : <>
-                     <button className="button"> <Link id='link' to="/user">User</Link></button>  
-                     <button className="button"><Link id='link' to="/myRoutines">MyRoutines</Link></button> 
-                     <button className="button"><Link id='link' to="/myActivities">MyActivities</Link></button>
-                     <button className="button"><Link id='link' to="/routines">Public Routines</Link></button> 
-                     <button className="button"><Link id='link' to="/activities">Public Activities</Link></button></>} </div>
+                    <button className="button"><Link id='link' to="/login">Login</Link></button>
+                    <button className="button"><Link id='link' to="/signUp">SignUp</Link></button>
+                    <button className="button"><Link id='link' to="/routines">Public Routines</Link></button> 
+                    <button className="button"><Link id='link' to="/activities">Public Activities</Link></button>
+                    </> : <>
+                    <button className="button"> <Link id='link' to="/user">User</Link></button>  
+                    <button className="button"><Link id='link' to="/myRoutines">MyRoutines</Link></button> 
+                    <button className="button"><Link id='link' to="/myActivities">MyActivities</Link></button>
+                    <button className="button"><Link id='link' to="/routines">Public Routines</Link></button> 
+                    <button className="button"><Link id='link' to="/activities">Public Activities</Link></button></>} </div>
                 </div>
+    
+                    <Route path="/login">
+                        <Login setLoggedIn={setLoggedIn}loggedIn={loggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>   
+                    </Route>
+
+                    <Route path="/signUp">
+                        <RegisterLogin setLoggedIn={setLoggedIn} loggedIn={loggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
+                    </Route>
+
+                    <Route path="/user">
+                        <User loggedIn={loggedIn} setLoggedIn={setLoggedIn} username={username} setUsername={setUsername}/>
+                    </Route> 
  
-                {/* <div id="myProfile"> */}
-
-                   
-                        <Route path="/login"><Login setLoggedIn={setLoggedIn}
-                                loggedIn={loggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
-                                
-                        </Route>
-
-                        <Route path="/signUp"><RegisterLogin setLoggedIn={setLoggedIn}
-                            loggedIn={loggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
-                        </Route>
-
-                        
-                        <Route path="/user"><User loggedIn={loggedIn} setLoggedIn={setLoggedIn} username={username} setUsername={setUsername}/>
-                            {/* <div>
-                                <Route path="/myRoutines"><MyRoutines name = {name} setName = {setName} goal = {goal} setGoal = {setGoal} isPublic = {isPublic} setIsPublic = {setIsPublic} routines = {routines} setRoutines = {setRoutines}/></Route>
-                            </div>
-                            <div>
-                                <Route path="/myActivities">< MyActivities activities = {activities} setActivities = {setActivities}/></Route>
-                            </div> */}
-
-                        </Route> 
-
-                    {/* </div> */}
-
-
-               
-             <Route path = "/routines"><Routines routines = {routines} setRoutines = {setRoutines}/></Route>  
-
+            <Route path = "/routines"><Routines routines = {routines} setRoutines = {setRoutines}/></Route>  
             <Route path = "/myRoutines"><MyRoutines name = {name} setName = {setName} goal = {goal} setGoal = {setGoal} isPublic = {isPublic} setIsPublic = {setIsPublic} routines = {routines} setRoutines = {setRoutines}/></Route>
-
             <Route path = "/activities"><Activities activities = {activities} setActivities = {setActivities}/></Route>
-
             <Route path="/myActivities">< MyActivities activities = {activities} setActivities = {setActivities}/></Route>
 
-
             </BrowserRouter>
-
-
         </div>);
         }
         export default App;
